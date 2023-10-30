d00ac2292fd94c0f9ec6c52b2f4f5a80c8335b64
andrei <146756436+Andrei-gorinov@users.noreply.github.com>
2023-10-30 08:51:54 +0300

    Update 8.2.py

--- zadanie_8/8.2.py
+++ zadanie_8/8.2.py
@@ -1,20 +1,25 @@
-  def check_point_in_circle(x, y, a, b, R):
-    distance = math.sqrt((x - a)**2 + (y - b)**2)
-   if distance <= R:
-    return True
-   else:
-    return False
+ import math
 
-   a = 0
-   b = 0
-   R = 5
+def check_point_in_circle(x, y, a, b, R):
+distance = math.sqrt((x - a)**2 + (y - b)**2)
+if distance <= R:
+return True
+else:
+return False
 
-   p1, p2 = 2, 2
-   f1, f2 = 4, 4
-   l1, l2 = -2, -2
+# Заданные параметры окружности
+a = 0
+b = 0
+R = 5
 
-   points = [(p1, p2), (f1, f2), (l1, l2)]
-   points_inside_circle = 0
+# Точки P, F и L
+p1, p2 = 2, 2
+f1, f2 = 4, 4
+l1, l2 = -2, -2
+
+# Проверяем, лежит ли каждая точка внутри окружности
+points = [(p1, p2), (f1, f2), (l1, l2)]
+points_inside_circle = 0
 
 for point in points:
 x, y = point
