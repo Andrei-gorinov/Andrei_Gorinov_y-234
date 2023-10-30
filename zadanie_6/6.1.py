c6ff2bfdf7c775fc9fd7685970da8d8ceda71dae
andrei <146756436+Andrei-gorinov@users.noreply.github.com>
2023-10-30 08:36:14 +0300

    Update 6.1.py

--- zadanie_6/6.1.py
+++ zadanie_6/6.1.py
@@ -1,1 +1,19 @@
+def replace_and_count(string):
+    count = 0  # переменная для подсчета количества замен
+    new_string = ""  # новая строка после замены
 
+    for char in string:
+        if char == "а":
+            new_string += "о"
+            count += 1
+        else:
+            new_string += char
+
+    return count, len(new_string)
+
+
+input_string = input("Введите строку: ")
+replacement_count, string_length = replace_and_count(input_string)
+
+print("Количество замен:", replacement_count)
+print("Количество символов в строке:", string_length)
