e1a9e5ceab9cea081596f123958010523f3d3208
andrei <146756436+Andrei-gorinov@users.noreply.github.com>
2023-12-02 22:04:54 +0300

    Update 8.2.py

--- zadanie_8/8.2.py
+++ zadanie_8/8.2.py
@@ -1,29 +1,30 @@
- import math
+def replace_elements(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] < 0:
+                matrix[i][j] = 0
+            elif matrix[i][j] > 0:
+                matrix[i][j] = 1
 
-def check_point_in_circle(x, y, a, b, R):
-distance = math.sqrt((x - a)**2 + (y - b)**2)
-if distance <= R:
-return True
-else:
-return False
+def print_lower_triangle(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1):
+            print(matrix[i][j], end=' ')
+        print()
 
-# Заданные параметры окружности
-a = 0
-b = 0
-R = 5
+# Пример использования функций
+matrix = [
+    [-1, 2, -3],
+    [4, -5, 6],
+    [-7, 8, -9]
+]
 
-# Точки P, F и L
-p1, p2 = 2, 2
-f1, f2 = 4, 4
-l1, l2 = -2, -2
+replace_elements(matrix)
+print("Матрица после замены элементов:")
+for row in matrix:
+    print(row)
 
-# Проверяем, лежит ли каждая точка внутри окружности
-points = [(p1, p2), (f1, f2), (l1, l2)]
-points_inside_circle = 0
-
-for point in points:
-x, y = point
-if check_point_in_circle(x, y, a, b, R):
-points_inside_circle += 1
-
-print("Количество точек, лежащих внутри окружности:", points_inside_circle)
+print("Нижняя треугольная матрица:")
+print_lower_triangle(matrix)
