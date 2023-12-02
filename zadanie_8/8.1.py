263dd87cbb62a30d76a0daba648cc8018e5f063f
andrei <146756436+Andrei-gorinov@users.noreply.github.com>
2023-12-02 22:03:51 +0300

    Update 8.1.py

--- zadanie_8/8.1.py
+++ zadanie_8/8.1.py
@@ -1,14 +1,35 @@
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+def find_rows_with_extreme_sums(matrix):
+    if not matrix:
+        return []
 
-def divide_fractions(a, b, c, d):
-    numerator = a * d
-    denominator = b * c
-    gcd_val = gcd(numerator, denominator)
+    max_sum_row = 0
+    min_sum_row = 0
+    max_sum = sum(matrix[0])
+    min_sum = sum(matrix[0])
 
-    numerator //= gcd_val
-    denominator //= gcd_val
+    for i in range(1, len(matrix)):
+        row_sum = sum(matrix[i])
 
-    return numerator, denominator
+        if row_sum > max_sum:
+            max_sum = row_sum
+            max_sum_row = i
+        elif row_sum < min_sum:
+            min_sum = row_sum
+            min_sum_row = i
+
+    return max_sum_row, min_sum_row, max_sum, min_sum
+
+
+# Пример использования функции
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+max_sum_row, min_sum_row, max_sum, min_sum = find_rows_with_extreme_sums(matrix)
+
+print("Строка с наибольшей суммой элементов:", matrix[max_sum_row])
+print("Сумма элементов строки:", max_sum)
+print("Строка с наименьшей суммой элементов:", matrix[min_sum_row])
+print("Сумма элементов строки:", min_sum)
