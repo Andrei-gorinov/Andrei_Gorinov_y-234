7d3574826f15e54318893cc593b2a362ba622530
andrei <146756436+Andrei-gorinov@users.noreply.github.com>
2023-12-04 00:41:23 +0300

    Create B2.py

---
+++ zadanie 12/B2.py
@@ -0,0 +1,13 @@
+def find_second_largest():
+    largest = -float('inf')
+    second_largest = -float('inf')
+    while True:
+        number = int(input("Введите число (0 для завершения): "))
+        if number == 0:
+            break
+        if number > largest:
+            second_largest = largest
+            largest = number
+        elif number > second_largest:
+            second_largest = number
+    return second_largest
