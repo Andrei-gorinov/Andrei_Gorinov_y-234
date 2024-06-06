7837b1e11c413207ddcae878e405d55709f655df
andrei <146756436+Andrei-gorinov@users.noreply.github.com>
2024-06-06 04:12:39 +0300

    Update z2.java

--- третья точка/z2.java
+++ третья точка/z2.java
@@ -2,24 +2,33 @@ import java.util.Scanner;
 
 public class FindSquareDifference {
 
+    private static final int MAX_INPUT = 100000;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
 
-        if (n <= 0 || n > 100000) {
-            System.out.println("Число должно быть в диапазоне от 0 до 100000");
+        if (n <= 0 || n > MAX_INPUT) {
+            printOutOfRangeError();
             return;
         }
 
         int lowerSquare = (int) Math.floor(Math.sqrt(n));
-
         int upperSquare = lowerSquare + 1;
 
         if (upperSquare * upperSquare - lowerSquare * lowerSquare == n) {
             System.out.println(n + " = " + upperSquare * upperSquare + " - " + lowerSquare * lowerSquare);
         } else {
-            System.out.println("Невозможно представить " + n + " как разность двух последовательных квадратов");
+            printNoSquareDifferenceFound(n);
         }
+    }
+
+    private static void printOutOfRangeError() {
+        System.out.println("Число должно быть в диапазоне от 0 до " + MAX_INPUT);
+    }
+
+    private static void printNoSquareDifferenceFound(int n) {
+        System.out.println("Невозможно представить " + n + " как разность двух последовательных квадратов");
     }
 }
