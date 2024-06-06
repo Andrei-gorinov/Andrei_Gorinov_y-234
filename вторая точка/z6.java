0600df8884ca33c57ba1491b5042d74731be1dd3
andrei <146756436+Andrei-gorinov@users.noreply.github.com>
2024-06-06 04:08:52 +0300

    Update z6.java

--- вторая точка/z6.java
+++ вторая точка/z6.java
@@ -4,37 +4,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] memoryCell = new int[3];
-        int index = 0;
+        int[] memoryCells = new int[3];
+        int currentIndex = 0;
 
         while (true) {
             System.out.println("Введите число (для выхода введите 0):");
-            int number = scanner.nextInt();
+            int inputNumber = scanner.nextInt();
 
-            if (number == 0) {
-                break; 
+            if (inputNumber == 0) {
+                break;
             }
 
-            if (index < memoryCell.length) {
-
-                memoryCell[index] = number;
-                index++;
+            if (currentIndex < memoryCells.length) {
+                memoryCells[currentIndex] = inputNumber;
+                currentIndex++;
             } else {
-              
-                int minIndex = findIndexOfMinValue(memoryCell);
-                if (number > memoryCell[minIndex]) {
-                    memoryCell[minIndex] = number;
+                int minIndex = findMinIndex(memoryCells);
+                if (inputNumber > memoryCells[minIndex]) {
+                    memoryCells[minIndex] = inputNumber;
                 }
             }
         }
 
         System.out.println("Результат:");
-        for (int value : memoryCell) {
+        for (int value : memoryCells) {
             System.out.print(value + " ");
         }
     }
 
-    public static int findIndexOfMinValue(int[] array) {
+    public static int findMinIndex(int[] array) {
         int minIndex = 0;
         for (int i = 1; i < array.length; i++) {
             if (array[i] < array[minIndex]) {
