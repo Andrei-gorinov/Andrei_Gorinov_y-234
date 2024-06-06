4d34d2e98a2e9f8f97f90c99a96c5d4bac5bbf40
andrei <146756436+Andrei-gorinov@users.noreply.github.com>
2024-06-06 04:13:14 +0300

    Update z3.java

--- третья точка/z3.java
+++ третья точка/z3.java
@@ -5,21 +5,32 @@ public class SumOfPowers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Введите число:");
         int number = input.nextInt();
+
+        System.out.println("Введите стартовую степень:");
         int startIndex = input.nextInt();
 
+        int sum = calculatePowerSum(number, startIndex);
+
+        boolean isPossible = isSumMultipleOf64(sum);
+
+        System.out.println(isPossible ? "Yes" : "No");
+    }
+
+    private static int calculatePowerSum(int number, int startIndex) {
         int sum = 0;
-        int digit;
         int power = startIndex;
         while (number > 0) {
-            digit = number % 10;
+            int digit = number % 10;
             sum += Math.pow(digit, power);
             number /= 10;
             power++;
         }
+        return sum;
+    }
 
-        boolean isPossible = sum % 64 == 0;
-
-        System.out.println(isPossible ? "Yes" : "No");
+    private static boolean isSumMultipleOf64(int sum) {
+        return sum % 64 == 0;
     }
 }
