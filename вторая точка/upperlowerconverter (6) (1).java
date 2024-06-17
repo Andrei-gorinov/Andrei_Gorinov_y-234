0c3a6285c565126eb89de94ee9a68359636d3e9f
andrei <146756436+Andrei-gorinov@users.noreply.github.com>
2024-06-17 15:40:53 +0300

    Update and rename z8.java to upperlowerconverter (6) (1).java

--- вторая точка/upperlowerconverter (6) (1).java
+++ вторая точка/upperlowerconverter (6) (1).java
@@ -4,13 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Ввод строки символов от пользователя
         System.out.println("Введите строку символов:");
         String input = scanner.nextLine();
 
+        // Преобразование строки и вывод результата
         String transformedString = transformString(input);
         System.out.println("Результат: " + transformedString);
+
+        // Закрытие сканера
+        scanner.close();
     }
 
+    // Метод для преобразования строки в зависимости от количества заглавных и строчных букв
     public static String transformString(String input) {
         int uppercaseCount = 0;
         int lowercaseCount = 0;
