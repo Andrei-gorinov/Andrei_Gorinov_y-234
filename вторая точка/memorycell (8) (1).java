455935333511765ca2e27c115acf36be361bea36
andrei <146756436+Andrei-gorinov@users.noreply.github.com>
2024-06-17 15:38:46 +0300

    Update memorycell (8) (1).java

--- вторая точка/memorycell (8) (1).java
+++ вторая точка/memorycell (8) (1).java
@@ -1,22 +1,55 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class MemoryCell {
+    private int[] numbers = new int[3];
+    private int numOfElements = 0;
 
-        System.out.println("Введите три числа: начальное значение, шаг и количество шагов:");
-        int startValue = scanner.nextInt(); 
-        int stepValue = scanner.nextInt();  
-        int totalSteps = scanner.nextInt(); 
-
-        generateSeries(startValue, stepValue, totalSteps);
+    // Метод для вставки числа в ячейку памяти
+    public void insertNumber(int num) {
+        if (numOfElements < 3) {
+            numbers[numOfElements] = num;
+            numOfElements++;
+        } else {
+            int minIndex = findMinIndex();
+            numbers[minIndex] = num;
+        }
     }
 
-    public static void generateSeries(int startValue, int stepValue, int totalSteps) {
-        int currentValue = startValue;
-        for (int i = 0; i < totalSteps; i++) {
-            System.out.print(currentValue + " ");
-            currentValue += stepValue;
+    // Метод для нахождения индекса минимального числа в массиве
+    public int findMinIndex() {
+        int minIndex = 0;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < numbers[minIndex]) {
+                minIndex = i;
+            }
         }
+        return minIndex;
+    }
+
+    // Метод для печати чисел, хранящихся в ячейке памяти
+    public void printNumbers() {
+        System.out.println("Числа в ячейке памяти:");
+        for (int i = 0; i < numOfElements; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MemoryCell memoryCell = new MemoryCell();
+
+        while (true) {
+            System.out.print("Введите число (или -1 для выхода): ");
+            int number = scanner.nextInt();
+            
+            if (number == -1) {
+                break;
+            }
+
+            memoryCell.insertNumber(number);
+            memoryCell.printNumbers();
+        }
+
+        scanner.close();
     }
 }
